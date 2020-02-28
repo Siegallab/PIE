e9dcf3094d976c31ed5999dfb86c754ed0e6d8fb
plavskin <eugene.plavskin@gmail.com>
2020-02-28 16:21:12 -0500

    Wrote PIE.colony_edge_detect._ColonyPropertyFinder to measure colony properties in a single image and record them in a pandas df; Wrote PIE.colony_edge_detect.create_color_overlay to create color overlays of masks over images; Wrote methods in PIE.colony_edge_detect._ImageAnalyzer to create and save necessary output files post-analysis; Wrote PIE.colony_edge_detect.read_and_run_analysis to read in files and run PIE colony edge detection with output writing (essentially a function for the web app)

--- PIE_python/PIE/colony_edge_detect.py
+++ PIE_python/PIE/colony_edge_detect.py
@@ -4,39 +4,53 @@
 Performs colony edge detection
 '''
 
+# !!! TODO: ANALYSIS NOT WORKING ON JPEG?!
+
 import cv2
+import csv
 from PIE import adaptive_threshold
 import os
 from PIE import ported_matlab
 import numpy as np
+import pandas as pd
+import copy
 
 class _ImageAnalyzer(object):
 	'''
 	Runs image analysis and creates necessary output files
 	'''
-
-	def __init__(self, input_im_path, output_path, image_type = 'brightfield',
+	# TODO: save all jpegs as jpeg2000s instead?
+	def __init__(self, original_im, image_name, output_path, image_type = 'brightfield',
 				hole_fill_area = np.inf, cleanup = False,
 				max_proportion_exposed_edge = 0.25,
 				save_extra_info = False, threshold_plot_width = 6,
 				threshold_plot_height = 4, threshold_plot_dpi = 200,
-				threshold_plot_filetype = 'jpg'):
+				threshold_plot_filetype = 'png',
+				create_pie_overlay = False):
+		# TODO: Reorganize this section to make more sense; add more info about inputs
 		# parameter for saving jpegs
-		self._jpeg_quality = 95
+		self._jpeg_quality = 98
 		# parameters for saving plots
 		self._threshold_plot_width = threshold_plot_width
 		self._threshold_plot_height = threshold_plot_height
 		self._threshold_plot_dpi = threshold_plot_dpi
 		self._threshold_plot_filetype = threshold_plot_filetype
 		self._save_extra_info = save_extra_info
-		self.input_im_path = input_im_path
+		self.original_im = original_im
 		self.image_type = image_type
+		self.hole_fill_area = hole_fill_area
 		self.cleanup = cleanup
+		self._create_pie_overlay = create_pie_overlay
 		self.max_proportion_exposed_edge = max_proportion_exposed_edge
 		# get name of image file without path or extension
-		self.image_name = os.path.splitext(os.path.basename(input_im_path))[0]
+		self.image_name = image_name
 		# set up folders
 		self._set_up_folders(output_path)
+		# set colors for boundary_im outputs
+		self._boundary_color = [255, 0, 255] # magenta
+		self._center_color = [145, 224, 47] # bright green
+		self._boundary_alpha = 1
+		self._cell_center_alpha = 0.75
 	
 	def _set_up_folders(self, output_path):
 		'''
@@ -45,23 +59,21 @@ class _ImageAnalyzer(object):
 		# !!! NEEDS UNITTEST
 		image_output_directories = \
 			['jpgGRimages', 'boundary_ims', 'colony_masks', 'threshold_plots',
-				'colony_center_overlays']
+				'colony_center_overlays', 'single_im_colony_properties']
 		self._image_output_dir_dict = dict()
 		for current_subdir in image_output_directories:
 			current_path = os.path.join(output_path, current_subdir)
-			self.image_output_dir_dict[current_subdir] = current_path
+			self._image_output_dir_dict[current_subdir] = current_path
 			if not os.path.exists(current_path):
 				os.makedirs(current_path)
 
-	def _read_image(self, input_im_path, image_type):
+	def _prep_image(self):
 		'''
 		Reads the image
 		If image_type is brightfield, reads as is
 		If image_type is phasecontrast, inverts image before analysis
 		'''
 		# !!! NEEDS UNITTEST
-		# read in original image
-		self.original_im = cv2.imread(input_im_path, cv2.IMREAD_ANYDEPTH)
 		if self.image_type == 'brightfield':
 			# the image that was read in is the one that will be
 			# processed
@@ -75,18 +87,8 @@ class _ImageAnalyzer(object):
 				"image_type must be either 'brightfield' or 'phasecontrast'")
 		# create a normalized image for display purposes
 		self.norm_im = \
-			cv2.normalize(self.original_im, None, alpha=0, beta=1,
+			cv2.normalize(self.original_im, None, alpha=0, beta=(2**8-1),
 				norm_type=cv2.NORM_MINMAX)
-
-	def _save_jpeg(self):
-		'''
-		Writes jpeg of original input image
-		'''
-		# !!! NEEDS UNITTEST
-		jpeg_path = os.path.join(self._image_output_dir_dict('jpgGRimages'),
-			self.image_name + '.jpg')
-		cv2.imwrite(jpeg_path, self.original_im,
-			[cv2.IMWRITE_JPEG_QUALITY, self._jpeg_quality])
 
 	def _write_threshold_plot(self, threshold_plot):
 		'''
@@ -96,7 +98,7 @@ class _ImageAnalyzer(object):
 		# !!! NEEDS UNITTEST
 		if threshold_plot is not None:
 			threshold_plot_file = \
-				os.path.join(self._image_output_dir_dict('threshold_plots'),
+				os.path.join(self._image_output_dir_dict['threshold_plots'],
 					self.image_name + '_threshold_plot.' + 
 					self._threshold_plot_filetype)
 		# write threshold plot
@@ -112,7 +114,7 @@ class _ImageAnalyzer(object):
 		'''
 		# !!! NEEDS UNITTEST
 		threshold_info_file = \
-			os.path.join(self._image_output_dir_dict('threshold_plots'),
+			os.path.join(self._image_output_dir_dict['threshold_plots'],
 					'threshold_info.csv')
 		threshold_info = [self.image_name, threshold_method_name, threshold]
 		if os.path.exists(threshold_info_file):
@@ -132,7 +134,7 @@ class _ImageAnalyzer(object):
 		self.cell_centers, threshold_method_name, threshold_plot, \
 			threshold, default_threshold_method_usage = \
 			adaptive_threshold.threshold_image(self.input_im,
-				self.save_extra_info)
+				self._save_extra_info)
 		# always save 'extra info' (boundary ims, threshold plots,
 		# combined cell center/boundary images) if default
 		# threshold_method was not used
@@ -152,26 +154,199 @@ class _ImageAnalyzer(object):
 			self.hole_fill_area, self.cleanup, self.max_proportion_exposed_edge)
 		self.colony_mask = pie_edge_detector.run_edge_detection()
 
+	def _save_jpeg(self):
+		'''
+		Writes jpeg of original input image
+		'''
+		# !!! NEEDS UNITTEST
+		jpeg_path = os.path.join(self._image_output_dir_dict['jpgGRimages'],
+			self.image_name + '.jpg')
+		# check whether original_im is > 8-bit; if so, decrease bitdepth
+		jpeg_im = copy.copy(self.original_im)
+		while np.max(jpeg_im) > 2**8:
+			jpeg_im = jpeg_im / 2**8
+		cv2.imwrite(jpeg_path, jpeg_im,
+			[cv2.IMWRITE_JPEG_QUALITY, self._jpeg_quality])
+
+	def _save_colony_mask(self):
+		'''
+		Saves colony mask as 8-bit tif file
+		'''
+		# !!! NEEDS UNITTEST
+		colony_mask_path = os.path.join(self._image_output_dir_dict['colony_masks'],
+			self.image_name + '.tif')
+		cv2.imwrite(colony_mask_path, np.uint8(self.colony_mask)*255)
+
+	def _save_boundary_im(self):
+		'''
+		Creates and saves an image in which the normalized image is
+		overlaid with a boundary (contour) of the colony mask
+		'''
+		colony_boundary_mask = ported_matlab.bwperim(self.colony_mask)
+		self.boundary_im = create_color_overlay(self.norm_im,
+			colony_boundary_mask, self._boundary_color, self._boundary_alpha)
+		boundary_im_path = \
+			os.path.join(self._image_output_dir_dict['boundary_ims'],
+				self.image_name + '.jpg')
+		cv2.imwrite(boundary_im_path, self.boundary_im,
+			[cv2.IMWRITE_JPEG_QUALITY, self._jpeg_quality])
+
+	def _save_colony_center_overlay(self):
+		'''
+		Creates and saves an image in which the normalized image is
+		overlaid with a boundary (contour) of the colony mask, as well
+		as the cell center mask
+		'''
+		self.colony_center_overlay = create_color_overlay(self.boundary_im,
+			self.cell_centers, self._center_color, self._cell_center_alpha)
+		colony_center_overlay_path = \
+			os.path.join(self._image_output_dir_dict['colony_center_overlays'],
+				self.image_name + '.jpg')
+		cv2.imwrite(colony_center_overlay_path, self.colony_center_overlay,
+			[cv2.IMWRITE_JPEG_QUALITY, self._jpeg_quality])
+
+	def _save_colony_properties(self):
+		'''
+		Measures and records area and centroid in every detected colony
+		'''
+		# !!! NEEDS UNITTEST !!!
+		colony_property_finder = _ColonyPropertyFinder(self.colony_mask)
+		self.colony_property_df = colony_property_finder.get_properties()
+		colony_property_path = \
+			os.path.join(self._image_output_dir_dict['single_im_colony_properties'],
+				self.image_name + '.csv')
+		colony_property_df_to_save = self.colony_property_df[['Area', 'Perimeter', 'cX', 'cY']]
+		colony_property_df_to_save.to_csv(colony_property_path, index = False)
+
 	def _save_post_edge_detection_files(self):
 		'''
-		Saves overlays, regionprops, and if necessary, bwperim, etc
+		Saves colony mask, jpeg, regionprops, and if necessary, boundary_ims, etc
 		'''
+		# write input image as a jpeg
+		self._save_jpeg()
+		# write colony mask as 8-bit tif file
+		self._save_colony_mask()
+		# measure and save colony properties
+		self._save_colony_properties()
+		# if self._save_extra_info is True, save additional files
+		if self._save_extra_info:
+			self._save_boundary_im()
+			self._save_colony_center_overlay()
 
 	def process_image(self):
 		'''
 		Performs all image processing
 		'''
-		# !!! NEEDS UNITTEST
+		# !!! NEEDS UNITTEST ???
 		# read in input image
-		self._read_image(self.input_im_path, self.image_type)
-		# write input image as a jpeg
-		self._save_jpeg()
+		self._prep_image()
 		# identify cell centers
 		self._find_cell_centers()
 		# identify colony mask
 		self._find_colony_mask()
 		# save analysis output files
 		self._save_post_edge_detection_files()
+		return(self.colony_mask, self.colony_property_df)
+
+class _ColonyPropertyFinder(object):
+	'''
+	Measures and holds properties of colonies based on a colony mask
+	'''
+	# !!! TODO: CHECK WHICH OTHER PARAMETERS ARE NEEDED DOWNSTREAM, RECORD THOSE TOO
+	def __init__(self, colony_mask):
+		self.colony_mask = colony_mask
+		self.property_df = pd.DataFrame()
+
+	def _find_connected_components(self):
+		'''
+		Finds connected components in a colony
+		'''
+		[self._label_num, self._labeled_mask, self._stat_matrix, self._centroids] = \
+			cv2.connectedComponentsWithStats(np.uint8(self.colony_mask),
+				True, True, True, 4, cv2.CV_32S)
+
+	def _find_areas(self):
+		'''
+		Records the areas of each colony
+		'''
+		areas = self._stat_matrix[:,4]
+		# remove background
+		self.property_df['Area'] = areas[1:]
+
+	def _find_centroids(self):
+		'''
+		Records centroids of each colony
+		'''
+		# remove background centroid
+		self.property_df['cX'] = self._centroids[1:,0]
+		self.property_df['cY'] = self._centroids[1:,1]
+
+	def _find_bounding_box(self):
+		'''
+		Records bounding box of each colony
+		'''
+		# !!! NEEDED???
+		# Can be found in self._stat_matrix
+		pass
+
+	def _find_flat_coordinates(self, single_colony_mask):
+		'''
+		Finds indices of True pixels in flattened single_colony_mask
+		Returns a string of indices concatenated separated by a space
+		(these can be read back into np array via np.fromstring)
+		'''
+		# indices in flattened current_mask
+		# can be used to reference indices in original array as
+		# current_mask.flat(np.fromstring(concat_coords,
+		#	dtype = int, sep = ' '))
+		flat_coords = np.flatnonzero(single_colony_mask)
+		# concatenate indices with space
+		concat_coords = ' '.join([str(k) for k in flat_coords])
+		return(concat_coords)
+
+	def _find_perimeter(self, single_colony_mask):
+		'''
+		Finds external perimeter of mask that should contain single
+		colony
+		NB: perimeter calculated here between centers of consecutive
+		contour pixels
+		'''
+		colony_cont = \
+			cv2.findContours(np.uint8(single_colony_mask), cv2.RETR_EXTERNAL,
+				cv2.CHAIN_APPROX_NONE)[0][0]
+		colony_perim = cv2.arcLength(colony_cont, True)
+		return(colony_perim)
+
+	def _find_colonywise_properties(self):
+		'''
+		Loops through detected colonies and measures the properties that
+		need to be measured one at a time
+		'''
+		self.property_df['Perimeter'] = np.nan
+		self.property_df['PixelIdxList'] = None
+		# don't loop through background
+		for colony in range(1, self._label_num):
+			current_colony_mask = self._labeled_mask == colony
+			self.property_df.at[colony-1, 'Perimeter'] = \
+				self._find_perimeter(current_colony_mask)
+			self.property_df.at[colony-1, 'PixelIdxList'] = \
+				self._find_flat_coordinates(current_colony_mask)
+
+	def _measure_and_record_properties(self):
+		'''
+		Measures and records all colony properties
+		'''
+		self._find_connected_components()
+		self._find_areas()
+		self._find_centroids()
+		self._find_colonywise_properties()
+
+	def get_properties(self):
+		'''
+		Returns dataframe of colony properties
+		'''
+		self._measure_and_record_properties()
+		return(self.property_df)
 
 class _EdgeDetector(object):
 	'''
@@ -223,7 +398,7 @@ class _EdgeDetector(object):
 		# them into a composite colony_mask
 		initial_colony_mask = np.zeros(self.input_im.shape, dtype = bool)
 		for pie_quad_name, pie_piece_quadrant in \
-			self.pie_piece_dict.iteritems():
+			self.pie_piece_dict.items():
 			current_cell_pieces = \
 				pie_piece_quadrant.id_center_overlapping_pie_pieces(
 					self.cell_centers)
@@ -279,6 +454,10 @@ class _EdgeDetector(object):
 			_filter_by_overlaps(mask_to_clear, mask_edge,
 				keep_overlapping_objects = False)
 		return(cleared_mask)
+
+	def draw_pie_pieces(self):
+		# TODO: write this method
+		pass
 
 	def run_edge_detection(self):
 		'''
@@ -357,6 +536,37 @@ def _filter_by_overlaps(mask_to_filter, guide_mask, keep_overlapping_objects):
 		np.isin(labeled_mask, allowed_labels)
 	return(filtered_mask)
 
+def create_color_overlay(image, mask, mask_color, mask_alpha):
+	'''
+	Creates an rbg image of image with mask overlaid on it in
+	mask_color with mask_alpha
+	mask_color is a list of r, g, b values on a scale of 0 to 255
+	'''
+	# check whether image is grayscale; if so, convert to rgb
+	color_image = copy.copy(image)
+	if len(np.shape(color_image)) == 2:
+		color_image = cv2.cvtColor(color_image, cv2.COLOR_GRAY2RGB)
+	# adjust mask_color by alpha, inverse, scale to image bitdepth
+	mask_color_adjusted_tuple = \
+		tuple(float(k)/255*np.max(color_image)*mask_alpha for k in mask_color[::-1])
+	color_image[mask] = \
+		np.round(color_image[mask].astype(float) * (1-mask_alpha) +
+			mask_color_adjusted_tuple).astype(int)
+	return(color_image)
+
+def read_and_run_analysis(input_im_path, output_path,
+	image_type, hole_fill_area, cleanup, max_proportion_exposed_edge,
+	save_extra_info = True):
+	'''
+	Reads image from input_im_path and runs PIE colony detection on it,
+	saving required files to output_path
+	'''
+	image = cv2.imread(input_im_path, cv2.IMREAD_ANYDEPTH)
+	image_name = os.path.splitext(os.path.basename(input_im_path))[0]
+	image_analyzer = _ImageAnalyzer(image, image_name, output_path,
+		image_type, hole_fill_area, cleanup, max_proportion_exposed_edge,
+		save_extra_info)
+	image_analyzer.process_image()
 
 # !!! NB: explain in doc that this assumes that there will never be two images with the same name being placed in the same output folder
 
---
+++ PIE_python/PIE_tests/test_ColonyPropertyFinder.py
@@ -0,0 +1,186 @@
+#!/usr/bin/python
+
+import unittest
+import numpy as np
+import pandas as pd
+from PIE.colony_edge_detect import _ColonyPropertyFinder
+from numpy.testing import assert_array_equal, assert_allclose
+from pandas.testing import assert_frame_equal
+
+colony_mask = np.array([
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 1, 1, 1, 0],
+	[0, 0, 0, 0, 1, 1, 1, 0],
+	[0, 1, 1, 1, 0, 1, 1, 0],
+	[0, 1, 0, 1, 0, 1, 0, 0],
+	[0, 1, 0, 1, 0, 0, 0, 0],
+	[0, 1, 1, 1, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0]], dtype = bool)
+
+single_colony_mask = np.array([
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 1, 1, 1, 0, 0, 0, 0],
+	[0, 1, 0, 1, 0, 0, 0, 0],
+	[0, 1, 0, 1, 0, 0, 0, 0],
+	[0, 1, 1, 1, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0]], dtype = bool)
+
+class TestFindConnectedComponents(unittest.TestCase):
+
+	def setUp(self):
+		self.col_prop_finder = _ColonyPropertyFinder(colony_mask)
+
+	def test_find_connected_components(self):
+		'''
+		Test that colony properties identified correctly
+		'''
+		expected_label_num = 3
+		expected_labeled_mask = np.array([
+			[0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 1, 1, 1, 0],
+			[0, 0, 0, 0, 1, 1, 1, 0],
+			[0, 2, 2, 2, 0, 1, 1, 0],
+			[0, 2, 0, 2, 0, 1, 0, 0],
+			[0, 2, 0, 2, 0, 0, 0, 0],
+			[0, 2, 2, 2, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0]])
+		# left top width height area
+		expected_stat_matrix = np.array([
+			[0, 0, 8, 8, 45],
+			[4, 1, 3, 4, 9],
+			[1, 3, 3, 4, 10]])
+		# centroids are mean of pixel indices
+		expected_centroids = np.array([
+			[158.0/45, 160.0/45],
+			[5+1.0/9, 2+1.0/9],
+			[2, 4.5]])
+		# run connected component finder
+		self.col_prop_finder._find_connected_components()
+		# check properties
+		assert_array_equal(expected_labeled_mask,
+			self.col_prop_finder._labeled_mask)
+		self.assertEqual(expected_label_num,
+			self.col_prop_finder._label_num)
+		assert_array_equal(expected_stat_matrix,
+			self.col_prop_finder._stat_matrix)
+		assert_allclose(expected_centroids,
+			self.col_prop_finder._centroids)
+
+class TestFindAreas(unittest.TestCase):
+
+	def test_find_areas(self):
+		'''
+		Tests that correct non-background areas saved into property_df
+		'''
+		self.col_prop_finder = _ColonyPropertyFinder(colony_mask)
+		self.col_prop_finder._find_connected_components()
+		self.col_prop_finder._find_areas()
+		expected_property_df = pd.DataFrame({'Area': [9, 10]}, dtype = 'int32')
+		assert_frame_equal(expected_property_df,
+			self.col_prop_finder.property_df)
+
+	def test_find_areas_0(self):
+		'''
+		Tests that no areas returned for empty colony mask
+		'''
+		self.col_prop_finder = \
+			_ColonyPropertyFinder(np.zeros(colony_mask.shape,
+				dtype = colony_mask.dtype))
+		self.col_prop_finder._find_connected_components()
+		self.col_prop_finder._find_areas()
+		expected_property_df = pd.DataFrame({'Area': []}, dtype = 'int32')
+		assert_frame_equal(expected_property_df,
+			self.col_prop_finder.property_df)
+
+class TestFindCentroids(unittest.TestCase):
+
+	def test_find_centroids(self):
+		'''
+		Tests that correct non-background centroids saved into
+		property_df
+		'''
+		self.col_prop_finder = _ColonyPropertyFinder(colony_mask)
+		self.col_prop_finder._find_connected_components()
+		self.col_prop_finder._find_centroids()
+		expected_property_df = \
+			pd.DataFrame({'cX': [5+1.0/9, 2], 'cY': [2+1.0/9, 4.5]})
+		assert_frame_equal(expected_property_df,
+			self.col_prop_finder.property_df)
+
+	def test_find_areas_0(self):
+		'''
+		Tests that no centroids returned for empty colony mask
+		'''
+		self.col_prop_finder = \
+			_ColonyPropertyFinder(np.zeros(colony_mask.shape,
+				dtype = colony_mask.dtype))
+		self.col_prop_finder._find_connected_components()
+		self.col_prop_finder._find_centroids()
+		expected_property_df = \
+			pd.DataFrame({'cX': [], 'cY': []})
+		assert_frame_equal(expected_property_df,
+			self.col_prop_finder.property_df)
+
+class TestFindFlatCoordinates(unittest.TestCase):
+
+	def test_find_flat_coordinates(self):
+		'''
+		Tests that flat list of indices identified can be used to
+		recapitulate original mask
+		'''
+		self.col_prop_finder = _ColonyPropertyFinder(colony_mask)
+		test_flat_coords = \
+			self.col_prop_finder._find_flat_coordinates(single_colony_mask)
+		test_mask = np.zeros(single_colony_mask.shape,
+			dtype = single_colony_mask.dtype)
+		test_mask.flat[np.fromstring(test_flat_coords,
+			dtype = int, sep = ' ')] = True
+		assert_array_equal(single_colony_mask, test_mask)
+
+class TestFindPerimeter(unittest.TestCase):
+
+	def test_find_perimeter(self):
+		'''
+		Tests that correct external colony perimeter returned
+		'''
+		self.col_prop_finder = _ColonyPropertyFinder(colony_mask)
+		expected_perimeter = 10.0
+		test_perimeter = \
+			self.col_prop_finder._find_perimeter(single_colony_mask)
+		self.assertEqual(expected_perimeter, test_perimeter)
+
+class TestFindColonyWiseProperties(unittest.TestCase):
+
+	def test_find_colonywise_properties(self):
+		'''
+		Tests that correct perimeter and colony pixel index string
+		identified
+		'''
+		self.col_prop_finder = _ColonyPropertyFinder(colony_mask)
+		self.col_prop_finder._find_connected_components()
+		self.col_prop_finder._find_colonywise_properties()
+		# perimeter value is counterintuitive here but makes sense when
+		# you realize contour tracks left to right first
+		expected_property_df = \
+			pd.DataFrame({'Perimeter': [6+np.sqrt(2)+np.sqrt(2), 10.0],
+				'PixelIdxList': ['12 13 14 20 21 22 29 30 37',
+					'25 26 27 33 35 41 43 49 50 51']})
+		assert_frame_equal(expected_property_df,
+			self.col_prop_finder.property_df)
+
+	def test_find_colonywise_properties_0(self):
+		'''
+		Tests that no centroids returned for empty colony mask
+		'''
+		pass
+		self.col_prop_finder = \
+			_ColonyPropertyFinder(np.zeros(colony_mask.shape,
+				dtype = colony_mask.dtype))
+		self.col_prop_finder._find_connected_components()
+		self.col_prop_finder._find_centroids()
+		expected_property_df = \
+			pd.DataFrame({'cx': [], 'cy': []})
+		#assert_frame_equal(expected_property_df,
+		#	self.col_prop_finder.property_df)
